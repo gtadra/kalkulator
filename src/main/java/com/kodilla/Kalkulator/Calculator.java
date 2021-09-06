450eb27dcbc8b0b1bcb7033745e3bd70c8af1208
Grzegorz T <gtadra@gmail.com>
2021-09-06 18:10:55 +0200

    first commit

--- src/main/java/com/kodilla/Kalkulator/Calculator.java
+++ src/main/java/com/kodilla/Kalkulator/Calculator.java
@@ -1,4 +1,19 @@
 package com.kodilla.Kalkulator;
 
 public class Calculator {
+
+    public void sum(int valueA, int valueB){
+        System.out.println(valueA + " + " + valueB + " = " + (valueA + valueB));
+    }
+
+    public void sub(int valueA, int valueB){
+        System.out.println(valueA + " - " + valueB + " = " + (valueA - valueB));
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.sum(5,10);
+        calculator.sub(5,10);
+    }
+
 }
